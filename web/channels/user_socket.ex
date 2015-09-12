1668f42d26cdec4f91f4d2a3fa89a59479138ee7
Darko Fabijan <darko@renderedtext.com>
2015-09-13 00:27:29 +0200

    Introducing room channel

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule Chatt.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "rooms:*", Chatt.RoomChannel
+  channel "rooms:*", Chatt.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
